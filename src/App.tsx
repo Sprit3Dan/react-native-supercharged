1e89f9546f9a22b77143644838c970b71922ef2f
Alexey Savelev <savelichalex93@gmail.com>
2018-04-17 11:39:29 +0300

    Basic structure

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import {
+		AppRegistry,
+		StyleSheet,
+		View,
+		Text,
+		Platform,
+} from 'react-native';
+
+const instructions = Platform.select({
+		ios: 'Press Cmd+R to reload,\n' +
+				 'Cmd+D or shake for dev menu',
+		android: 'Double tap R on your keyboard to reload,\n' +
+						 'Shake or press menu button for dev menu',
+});
+
+export class App extends React.Component<undefined, State> {
+		public render(): JSX.Element {
+				return (
+						<View style={styles.container}>
+								<Text style={styles.welcome}>
+										Welcome to React Native!
+								</Text>
+								<Text style={styles.instructions}>
+										To get started, edit App.js
+								</Text>
+								<Text style={styles.instructions}>
+										{instructions}
+								</Text>
+						</View>
+				);
+		}
+}
+
+const styles = StyleSheet.create({
+		container: {
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: '#F5FCFF',
+		},
+		welcome: {
+				fontSize: 20,
+				textAlign: 'center',
+				margin: 10,
+		},
+		instructions: {
+				textAlign: 'center',
+				color: '#333333',
+				marginBottom: 5,
+		},
+});
+
+AppRegistry.registerComponent('HelloWorld', () => App);
